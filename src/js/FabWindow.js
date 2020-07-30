6d9c14d75994d0598fa9c2669af163b08d9962d5
fabien <fabien.winkler@outlook.fr>
2020-07-30 17:59:50 +0200

    try/catch , ajout show/hide

--- src/js/FabWindow.js
+++ src/js/FabWindow.js
@@ -25,7 +25,6 @@ var FabWindow = null,
         reduce: '.reduce',
         close: '.close',
         body: '.fab-content',
-        resizer: '.resizer',
         loader: '.loader',
       },
       effects: {
@@ -34,8 +33,6 @@ var FabWindow = null,
       },
       width: '800px',
       height: 'auto',
-      draggable: false,
-      resizable: false,
       maximizable: false,
       minimizable: false,
       title: '',
@@ -43,13 +40,17 @@ var FabWindow = null,
       loader: '<div class="loader"></div>',
 
       // function
-      onResize: function (fabWindow) { }
+      onResize: function (fabWindow) { },
+      onFullScreen: function (fabWindow) { }
     };
+
     this.isFullScreen = false,
       this.isMinimized = false,
       this.options = Object.assign(defaults, options);
+
     this.initialize(this.options);
-    return this;
+
+
   };
 
   /**
@@ -68,7 +69,6 @@ var FabWindow = null,
     this.$reduce = this.$el.querySelector(options.selectors.reduce);
     this.$maximize = this.$el.querySelector(options.selectors.maximize)
     this.$close = this.$el.querySelector(options.selectors.close);
-    this.$resizer = this.$el.querySelector(options.selectors.resizer);
     this.$body = this.$el.querySelector(options.selectors.body);
 
     $body.appendChild(this.$el);
@@ -76,7 +76,7 @@ var FabWindow = null,
     this.setContent('body', options.bodyContent);
 
     this.initHandlers();
-
+    this.show();
 
     (function updateTimer() {
       that.recalcLayout();
@@ -88,97 +88,118 @@ var FabWindow = null,
    * @function createWindow This function (like this name saying..) literally construct the html window
    */
   FabWindow.prototype.createWindow = function () {
-    var fabWindow = document.createElement('div');
-    fabWindow.className = 'fab-window ' + this.options.effects.in + ' transition';
-    fabWindow.id = this.options.id;
-    fabWindow.style.maxWidth = this.options.width;
-    fabWindow.style.maxHeight = this.options.height;
+    try {
+      var fabWindow = document.createElement('div');
+      fabWindow.className = 'fab-window ' + this.options.effects.in + ' transition';
+      fabWindow.id = this.options.id;
+      fabWindow.style.maxWidth = this.options.width;
+      fabWindow.style.maxHeight = this.options.height;
 
-    if (this.options.resizable) {
-      var fabWindowResizer = document.createElement('span');
-      fabWindowResizer.className = 'resizer';
-      fabWindow.appendChild(fabWindowResizer);
+      if (this.options.resizable) {
+        var fabWindowResizer = document.createElement('span');
+        fabWindowResizer.className = 'resizer';
+        fabWindow.appendChild(fabWindowResizer);
+      }
+
+      var fabWindowHeader = document.createElement('div');
+      fabWindowHeader.className = 'fab-header';
+      fabWindow.appendChild(fabWindowHeader);
+
+      var fabWindowTitle = document.createElement('h1');
+      fabWindowTitle.className = 'fab-title';
+      fabWindowTitle.innerHTML = this.options.title;
+      fabWindowHeader.appendChild(fabWindowTitle);
+
+      var fabWindowIcons = document.createElement('div');
+      fabWindowIcons.className = 'fab-icons';
+      fabWindowHeader.appendChild(fabWindowIcons)
+
+      if (this.options.minimizable) {
+        var fabReduceWindow = document.createElement('button');
+        fabReduceWindow.className = 'reduce';
+        fabWindowIcons.appendChild(fabReduceWindow)
+      }
+
+      if (this.options.maximizable) {
+        var fabMaximizeWindow = document.createElement('button');
+        fabMaximizeWindow.className = 'maximize';
+        fabWindowIcons.appendChild(fabMaximizeWindow)
+      }
+
+      var fabCloseWindow = document.createElement('button');
+      fabCloseWindow.className = 'close'
+      fabWindowIcons.appendChild(fabCloseWindow);
+
+      var fabWindowBody = document.createElement('div');
+      fabWindowBody.className = 'fab-content fade-in';
+      fabWindowBody.style.maxWidth = this.options.width;
+      fabWindowBody.style.maxHeight = this.options.height;
+      fabWindow.appendChild(fabWindowBody);
+
+      if (!document.querySelector(this.options.selectors.overlay)) {
+        var fabWindowOverlay = document.createElement('div');
+        fabWindowOverlay.className = 'fab-overlay fade-in';
+        $body.appendChild(fabWindowOverlay);
+      }
+
+      // On retire la class après l'affichage de la window pour plus de propreté
+      var that = this;
+      setTimeout(function () {
+        fabWindow.classList.remove(that.options.effects.in);
+      }, 1000)
+
+      return fabWindow;
+    } catch (error) {
+      console.error(error)
     }
-
-    var fabWindowHeader = document.createElement('div');
-    fabWindowHeader.className = 'fab-header';
-    fabWindow.appendChild(fabWindowHeader);
-
-    var fabWindowTitle = document.createElement('h1');
-    fabWindowTitle.className = 'fab-title';
-    fabWindowTitle.innerHTML = this.options.title;
-    fabWindowHeader.appendChild(fabWindowTitle);
-
-    var fabWindowIcons = document.createElement('div');
-    fabWindowIcons.className = 'fab-icons';
-    fabWindowHeader.appendChild(fabWindowIcons)
-
-    if (this.options.minimizable) {
-      var fabReduceWindow = document.createElement('button');
-      fabReduceWindow.className = 'reduce';
-      fabWindowIcons.appendChild(fabReduceWindow)
-    }
-
-    if (this.options.maximizable) {
-      var fabMaximizeWindow = document.createElement('button');
-      fabMaximizeWindow.className = 'maximize';
-      fabWindowIcons.appendChild(fabMaximizeWindow)
-    }
-
-    var fabCloseWindow = document.createElement('button');
-    fabCloseWindow.className = 'close'
-    fabWindowIcons.appendChild(fabCloseWindow);
-
-    var fabWindowBody = document.createElement('div');
-    fabWindowBody.className = 'fab-content fade-in';
-    fabWindowBody.style.maxWidth = this.options.width;
-    fabWindowBody.style.maxHeight = this.options.height;
-    fabWindow.appendChild(fabWindowBody);
-
-    var fabWindowOverlay = document.createElement('div');
-    fabWindowOverlay.className = 'fab-overlay fade-in';
-
-    $body.appendChild(fabWindowOverlay);
-
-
-    // On retire la class après l'affichage de la window pour plus de propreté
-    var that = this;
-    setTimeout(function () {
-      fabWindow.classList.remove(that.options.effects.in);
-    }, 1000)
-
-    return fabWindow;
   };
 
   FabWindow.prototype.initHandlers = function () {
-    var that = this;
+    try {
+      var that = this;
 
-    if (this.options.maximizable) {
-      this.$maximize.onclick = null
-      this.$maximize.addEventListener('click', function (e) {
+      // FullScreen event
+      if (this.options.maximizable) {
+        this.$maximize.onclick = null
+        this.$maximize.addEventListener('click', function (e) {
+          e.stopPropagation();
+          e.preventDefault();
+
+          that.toggleFullScreen();
+        })
+      }
+
+      // Close event
+      this.$close.onclick = null;
+      this.$close.addEventListener('click', function (e) {
         e.stopPropagation();
         e.preventDefault();
 
-        that.toggleFullScreen();
+        that.closeWindow();
       })
+
+      // Overlay close on click
+      this.$overlay.onclick = null;
+      this.$overlay.addEventListener('click', function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        that.closeWindow();
+      })
+    } catch (error) {
+      console.error(error)
     }
 
-    this.$close.onclick = null;
-    this.$close.addEventListener('click', function (e) {
-      e.stopPropagation();
-      e.preventDefault();
+  };
 
-      that.closeWindow();
-    })
+  FabWindow.prototype.show = function () {
+    this.$el.style.display = 'block';
+    this.$overlay.style.display = 'block';
+  };
 
-    this.$overlay.onclick = null;
-    this.$overlay.addEventListener('click', function (e) {
-      e.stopPropagation();
-      e.preventDefault();
-
-      that.closeWindow();
-    })
-
+  FabWindow.prototype.hide = function () {
+    this.$el.style.display = 'none';
+    this.$overlay.style.display = 'none';
   };
 
   FabWindow.prototype.toggleFullScreen = function () {
@@ -189,6 +210,10 @@ var FabWindow = null,
       this.isFullScreen = true
       this.$el.dispatchEvent(new CustomEvent("fullScreen"));
       this.$el.classList.add('fullScreen');
+
+      if (typeof this.options.onFullScreen === 'function') {
+        this.options.onFullScreen();
+      }
     }
   };
 
@@ -198,17 +223,20 @@ var FabWindow = null,
    * @param {string} content Content to append
    */
   FabWindow.prototype.setContent = function (target, content) {
-    if (content !== '' && content !== 'undefined' && content !== null) {
-      if ((!target && target === '' && target === null)) {
-        this.$body.innerHTML = content;
-      } else {
-        target = '$' + target;
-        this[target].innerHTML = content;
+    try {
+      if (content !== '' && content !== 'undefined' && content !== null) {
+        if ((!target && target === '' && target === null)) {
+          this.$body.innerHTML = content;
+        } else {
+          target = '$' + target;
+          this[target].innerHTML = content;
+        }
+        if (this.options.height === 'auto') {
+          this.options.height = this.$el.clientHeight;
+        }
       }
-      if (this.options.height === 'auto') {
-        this.options.height = this.$el.clientHeight;
-      }
-      //this.$body.style.height = parseInt(this.options.height) - this.$header.clientHeight + 'px';
+    } catch (error) {
+      console.error(error)
     }
   };
 
@@ -244,51 +272,52 @@ var FabWindow = null,
   };
 
   FabWindow.prototype.recalcLayout = function () {
-    var fabContentHeight = this.$body.scrollHeight,
-      modalHeight = this.$el.clientHeight,
-      windowHeight = $window.innerHeight,
-      wrapperHeight = this.$el.clientHeight - this.$header.clientHeight,
-      outerHeight = fabContentHeight + this.$header.clientHeight;
-    // var 
+    try {
+      var fabContentHeight = this.$body.scrollHeight,
+        modalHeight = this.$el.clientHeight,
+        windowHeight = $window.innerHeight,
+        wrapperHeight = this.$el.clientHeight - this.$header.clientHeight,
+        outerHeight = fabContentHeight + this.$header.clientHeight;
 
-    if (this.options.onResize && typeof this.options.onResize === 'function') {
-      this.options.onResize(this);
-    }
-
-    if ($window.innerWidth <= 600) {
-      this.isFullScreen = true;
-      this.$el.classList.add('fullScreen');
-    } else {
-      this.isFullScreen = false;
-      this.$el.classList.remove('fullScreen');
-    }
-
-    if (!this.isFullScreen) {
-      this.$el.style.height = parseInt(fabContentHeight) + (this.$header.clientHeight - 3) + 'px';
-    }
-
-    if (outerHeight > windowHeight) {
-      if (document.querySelectorAll('.fab-window').length === 1) {
-        document.querySelector('html').style.overflow = 'hidden';
+      if (this.options.onResize && typeof this.options.onResize === 'function') {
+        this.options.onResize(this);
       }
-      this.$el.style.height = windowHeight + 'px';
 
-    } else {
-      this.$el.style.height = fabContentHeight + (this.$header.clientHeight + 3) + 'px';
-      if (document.querySelectorAll('.fab-window').length === 1) {
-        document.querySelector('html').style.overflow = '';
-      }
-    }
-    var that = this;
-    (function applyScroll(){
-      if(fabContentHeight > wrapperHeight && outerHeight > windowHeight){
-        that.$body.classList.add('hasScroll');
-        that.$body.style.height = modalHeight - (that.$header.clientHeight+3) + 'px';
+      if ($window.innerWidth <= 600) {
+        this.$maximize.style.display = 'none';
       } else {
-        that.$body.classList.remove('hasScroll');
-        that.$body.style.height = 'auto';
+        this.$maximize.style.display = 'block';
       }
-  })();
+
+      if (!this.isFullScreen) {
+        this.$el.style.height = parseInt(fabContentHeight) + (this.$header.clientHeight - 3) + 'px';
+      }
+
+      if (outerHeight > windowHeight) {
+        if (document.querySelectorAll('.fab-window').length === 1) {
+          document.querySelector('html').style.overflow = 'hidden';
+        }
+        this.$el.style.height = windowHeight + 'px';
+
+      } else {
+        this.$el.style.height = fabContentHeight + (this.$header.clientHeight + 3) + 'px';
+        if (document.querySelectorAll('.fab-window').length === 1) {
+          document.querySelector('html').style.overflow = '';
+        }
+      }
+      var that = this;
+      (function applyScroll() {
+        if (fabContentHeight > wrapperHeight && outerHeight > windowHeight) {
+          that.$body.classList.add('hasScroll');
+          that.$body.style.height = modalHeight - (that.$header.clientHeight + 3) + 'px';
+        } else {
+          that.$body.classList.remove('hasScroll');
+          that.$body.style.height = 'auto';
+        }
+      })();
+    } catch (error) {
+      console.error(error)
+    }
   };
 
 })()
